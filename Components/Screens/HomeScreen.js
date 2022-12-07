5a90dff5ce7911cdacfa3abc25141e39436ef8df
Ahmad Naeem <ahmadnaeem26831@gmail.com>
2022-12-08 00:00:11 +0500

    work on homePage

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -14,6 +14,8 @@ import {
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import * as Animatable from "react-native-animatable";
+import { SearchBar } from "react-native-elements";
+
 import {
   TextField,
   FilledTextField,
@@ -25,24 +27,182 @@ const HomeScreen = ({ navigation }) => {
   return (
     <ViewStyle>
       <KeyboardAvoidingView>
+        <SearchBar placeholder="Type here..." />
         <ScrollView horizontal={true}>
-          <TouchableOpacity style={{ flexDirection: "row", height: 250 }}>
-            <ContainerBox>
-              <ImageLogo source={require("../../assets/woman-clothes.png")} />
-            </ContainerBox>
-          </TouchableOpacity>
+          <ContainerView style={{ flexDirection: "row" }}>
+            <Container>
+              <TextView>
+                <Text
+                  style={{
+                    padding: 10,
+                    fontSize: 18,
+                    marginTop: -5,
+                  }}
+                >
+                  4.5
+                </Text>
+                <ImageLogo
+                  style={{ marginTop: 8, marginLeft: -5 }}
+                  source={require("../../assets/star.png")}
+                />
+                <ImageLogo
+                  style={{ marginTop: 9, marginLeft: 100 }}
+                  source={require("../../assets/heart.png")}
+                />
+              </TextView>
+              <ImageLogo source={require("../../assets/man.png")} />
+              <ContainerValue>
+                <Text
+                  style={{
+                    padding: 10,
+                    fontSize: 18,
+                    fontWeight: "bold",
+                  }}
+                >
+                  Suit
+                </Text>
+                <TextView>
+                  <Text
+                    style={{
+                      padding: 10,
+                      marginTop: -10,
+                      fontSize: 18,
+                      fontWeight: "bold",
+                      color: "#ED1C22",
+                    }}
+                  >
+                    20,000
+                  </Text>
+                  <Text
+                    style={{
+                      padding: 10,
+                      fontSize: 13,
+                      marginTop: -8,
+                      marginLeft: 46,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    20% OFF
+                  </Text>
+                </TextView>
+              </ContainerValue>
+            </Container>
 
-          <TouchableOpacity style={{ flexDirection: "row", height: 250 }}>
-            <ContainerBox>
-              <ImageLogo source={require("../../assets/woman-clothes.png")} />
-            </ContainerBox>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={{ flexDirection: "row", height: 250 }}>
-            <ContainerBox>
-              <ImageLogo source={require("../../assets/woman-clothes.png")} />
-            </ContainerBox>
-          </TouchableOpacity>
+            <Container>
+              <TextView>
+                <Text
+                  style={{
+                    padding: 10,
+                    fontSize: 18,
+                    marginTop: -5,
+                  }}
+                >
+                  4.5
+                </Text>
+                <ImageLogo
+                  style={{ marginTop: 8, marginLeft: -5 }}
+                  source={require("../../assets/star.png")}
+                />
+                <ImageLogo
+                  style={{ marginTop: 9, marginLeft: 100 }}
+                  source={require("../../assets/heart.png")}
+                />
+              </TextView>
+              <ImageLogo source={require("../../assets/suit.png")} />
+              <ContainerValue>
+                <Text
+                  style={{
+                    padding: 10,
+                    fontSize: 18,
+                    fontWeight: "bold",
+                  }}
+                >
+                  Suit
+                </Text>
+                <TextView>
+                  <Text
+                    style={{
+                      padding: 10,
+                      marginTop: -10,
+                      fontSize: 18,
+                      fontWeight: "bold",
+                      color: "#ED1C22",
+                    }}
+                  >
+                    20,000
+                  </Text>
+                  <Text
+                    style={{
+                      padding: 10,
+                      fontSize: 13,
+                      marginTop: -8,
+                      marginLeft: 46,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    20% OFF
+                  </Text>
+                </TextView>
+              </ContainerValue>
+            </Container>
+            <Container>
+              <TextView>
+                <Text
+                  style={{
+                    padding: 10,
+                    fontSize: 18,
+                    marginTop: -5,
+                  }}
+                >
+                  4.5
+                </Text>
+                <ImageLogo
+                  style={{ marginTop: 8, marginLeft: -5 }}
+                  source={require("../../assets/star.png")}
+                />
+                <ImageLogo
+                  style={{ marginTop: 9, marginLeft: 100 }}
+                  source={require("../../assets/heart.png")}
+                />
+              </TextView>
+              <ImageLogo source={require("../../assets/jacket.png")} />
+              <ContainerValue>
+                <Text
+                  style={{
+                    padding: 10,
+                    fontSize: 18,
+                    fontWeight: "bold",
+                  }}
+                >
+                  Suit
+                </Text>
+                <TextView>
+                  <Text
+                    style={{
+                      padding: 10,
+                      marginTop: -10,
+                      fontSize: 18,
+                      fontWeight: "bold",
+                      color: "#ED1C22",
+                    }}
+                  >
+                    20,000
+                  </Text>
+                  <Text
+                    style={{
+                      padding: 10,
+                      fontSize: 13,
+                      marginTop: -8,
+                      marginLeft: 46,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    20% OFF
+                  </Text>
+                </TextView>
+              </ContainerValue>
+            </Container>
+          </ContainerView>
         </ScrollView>
       </KeyboardAvoidingView>
     </ViewStyle>
@@ -51,22 +211,33 @@ const HomeScreen = ({ navigation }) => {
 
 const ViewStyle = styled.View`
   flex: 1;
-  background-color: #ffffff;
-`;
-const ContainerBox = styled.Text`
-  margin-top: 20px;
-  border-radius: 20px;
-  background-color: #f5f5f5;
-  margin-left: 10px;
-  padding: 0px;
-  width: 150px;
+  background-color: #f0f8ff;
 `;
 
 const TextStyle = styled.Text`
   font-size: 24px;
 `;
 
+const Container = styled.View`
+  margin-top: 25px;
+  margin-left: 15px;
+  border-radius: 20px;
+  background-color: #f5f5dc;
+  height: 238px;
+  width: 180px;
+`;
 const ImageLogo = styled.Image`
-  margin-top: -20px;
+  margin-top: -1px;
+  margin-left: 25px;
+`;
+const ContainerView = styled.View``;
+const TextView = styled.View`
+  flex-direction: row;
+`;
+
+const ContainerValue = styled.View`
+  background-color: #ffffff;
+  border-radius: 10px;
+  margin-top: 5px;
 `;
 export default HomeScreen;
